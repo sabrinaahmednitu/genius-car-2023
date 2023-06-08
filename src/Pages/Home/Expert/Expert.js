a54309b7b91036296b0ea2dbbcb574fadce18c80
sabrina <sabrinaahmednitu@gmail.com>
2023-06-09 00:53:32 +0600

    expert section responsive

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -3,17 +3,17 @@ import './Expert.css';
 const Expert = ({ expert }) => {
   const { name, img } = expert;
   return (
-    <div class="card col-sm-12 col-md-6 col-lg-4 jx-5" style={{ width: '18rem' ,margin:"auto" }}>
-      <img src={img} class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">{name}</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Booking
-        </a>
+    <div className="col-sm-12 col-md-6 col-lg-4 g-5">
+      <div class="card " style={{ width: '18rem', margin: 'auto' }}>
+        <img src={img} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{name}</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <button class="service-btn"> Booking</button>
+        </div>
       </div>
     </div>
   );
