77af65d4cb04d408cd016732288be0a456289429
sabrina <sabrinaahmednitu@gmail.com>
2023-08-01 02:40:07 +0600

    successfully added a service

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import { useForm } from "react-hook-form"
-
+import { useForm } from 'react-hook-form';
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm()
-  const onSubmit = (data) => console.log(data)
-    return (
-      <div className="container">
-      
-          <h2 className='text-center mt-5' >Add new service here</h2>
+  const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+            .then(res => res.json())
+        .then(result=>console.log(result))
+    };
+    
 
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="d-flex justify-content-center align-items-center flex-column mt-5"
-          >
-            <input
-              {...register('firstName', { required: true, maxLength: 20 })}
-            />
-            <br />
-            <input {...register('lastName', { pattern: /^[A-Za-z]+$/i })} />
-            <br />
-            <input type="number" {...register('age', { min: 18, max: 99 })} />
-            <br />
-            <input type="submit" />
-          </form>
-      
-      </div>
-    );
+
+
+  return (
+    <div className="container">
+      <h2 className="text-center mt-5">Add new service here</h2>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="d-flex flex-column w-50 mx-auto mt-5 gap-4"
+      >
+        <input
+          placeholder="name"
+          {...register('name', { required: true, maxLength: 20 })}
+        />
+        <textarea placeholder="description" {...register('description')} />
+        <input placeholder="price" type="number" {...register('price')} />
+        <input placeholder="Photo URL" type="text" {...register('img')} />
+        <input className="btn btn-secondary" type="submit" ></input>
+      </form>
+    </div>
+  );
 };
 
 export default AddService;
