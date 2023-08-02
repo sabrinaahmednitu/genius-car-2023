da07ac0650545cd461ebfcc26c3b92555d9a2755
sabrina <sabrinaahmednitu@gmail.com>
2023-08-02 20:52:56 +0600

    detail page done

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import UseServiceDetail from '../../hooks/UseServiceDetail';
-
+import './ServiceDetail.css' 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
   
@@ -10,7 +10,20 @@ const ServiceDetail = () => {
 
     return (
       <div className=" mt-5">
-        <h3 className="text-center">Detail about Service : {service.name} </h3>
+        <h1 className="text-center">{serviceId}</h1>
+        <p className="text-center">Detail about {service.name} Service </p>
+
+        <div className=" w-50 mx-auto detailCards mt-5 mb-5">
+          <div className=" detailCard card mx-auto border p-3">
+            <img className="w-100" src={service.img} alt="" />
+            <div className="detailContent text-center">
+              <h2 className="mt-4">Name : {service.name}</h2>
+              <p>{service.description}</p>
+              <h3>price : ${service.price}</h3>
+            </div>
+          </div>
+        </div>
+
         <div className="text-center">
           <Link to={`/checkout/${serviceId}`}>
             <button className="btn btn-info">Checkout</button>
